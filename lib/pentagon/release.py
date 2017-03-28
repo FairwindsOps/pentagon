01a84fcd679071ff5d871df1152f7f7b3ada286c
EJ Etherington <ej@reactiveops.com>
2017-03-28 13:35:00 -0700

    fixing auto installation of requirements. Removed the import line to retreive the version and author string, replaced it with execfile which reads the variables in without requiring loading the modules. This gets around the race condition of needing dependancies installed before setup can be run. Removed unnecessary requirements.txt. Updated release file to only be the variables with no imports

--- lib/pentagon/release.py
+++ lib/pentagon/release.py
@@ -1,5 +1,3 @@
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 __version__ = '0.0.1'
-__author__  = 'ReactiveOps, Inc.'
+__author__ = 'ReactiveOps, Inc.'
