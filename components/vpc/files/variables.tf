82a9fec46dec1bd2dbe02338f5d9ebbb2be4dd10
Justin Mound <justinmound@gmail.com>
2017-04-26 14:18:56 -0400

    adding terraform-vpc as a proof of concept for pentagon components

---
+++ components/vpc/files/variables.tf
@@ -0,0 +1,50 @@
+###
+# AWS account specific variables
+###
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "aws_azs" {
+  default = "us-east-1a, us-east-1c, us-east-1d, us-east-1e"
+}
+
+###
+# Organization specific variables
+###
+
+variable "vpc_cidr" {
+  default = "10.10"
+}
+
+variable "aws_vpc_name" {}
+###
+# VPC module specific variables
+###
+
+variable "az_count" {
+  default = 3
+}
+variable "vpc_cidr_base" {}
+
+variable "admin_subnet_parent_cidr" {}
+variable "admin_subnet_cidrs" {
+  default = {}
+}
+
+variable "public_subnet_parent_cidr" {}
+variable "public_subnet_cidrs" {
+  default = {}
+}
+
+variable "private_prod_subnet_parent_cidr" {}
+variable "private_prod_subnet_cidrs" {
+  default = {}
+}
+
+variable "private_working_subnet_parent_cidr" {}
+variable "private_working_subnet_cidrs" {
+  default = {}
+}
