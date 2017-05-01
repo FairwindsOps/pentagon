735f4e8bb734a76f7ec636b6a345dceb4876aeb0
Justin Mound <justinmound@gmail.com>
2017-05-01 12:58:14 -0400

    moving workstation playbook and files from to its own repo. #179

--- workstation/files/pentagon-helpers.sh
+++
@@ -1,7 +0,0 @@
-function enable-virtualenvwrapper {
-    export WORKON_HOME=$HOME/workspace/venvs
-    export PROJECT_HOME=$HOME/workspace/projects
-    export VIRTUALENVWRAPPER_HOOK_DIR=$HOME/workspace/hooks
-
-    source /usr/local/bin/virtualenvwrapper.sh
-}
