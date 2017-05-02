d424152cfa6e9c530eaf529f036b0365949c34a7
Justin Mound <justinmound@gmail.com>
2017-05-02 17:15:48 -0400

    WIP

--- lib/pentagon/config/local/env-vars.sh
+++ lib/pentagon/config/local/env-vars.sh
@@ -4,21 +4,38 @@
 # iterate through an array defined here, set an uuper cased env var for each key, as defined in vars.yml
 # requires shyaml from https://github.com/0k/shyaml
 
-# path_to_yaml_vars=/Users/justin/Documents/work/reactive/workspace/projects/pentagon-proposal/pentagon-proposal-infrastructure/config/local/vars.yml
-path_to_yaml_vars="vars.yml"
 
-LIST_OF_VARIABLES=( "vpc_name" "aws_region" )
 
-# for key in  ${LIST_OF_VARIABLES[@]}; do
-#   # upper casing
-#   var=$(echo $key | awk '{print toupper($0)}')
-#   # set the env var
-#   export $var="$(cat $path_to_yaml_vars | shyaml get-value $key)"
-# done
+path_to_yaml_vars="${INFRASTRUCTURE_REPO}/config/local/vars.yml"
 
+LIST_OF_VARIABLES=( "vpc_name" "aws_region" "ansible_config" )
+LIST_OF_SECRETS=( "aws_secret_key" )
+
+
+set_vars() {
 for key in  ${LIST_OF_VARIABLES[@]}; do
-  # upper casing
-  var=$(echo $key | awk '{print toupper($0)}')
-  # unset the env var
-  unset $var
+  # converting to upper case
+  upper_case_key=$(echo $key | awk '{print toupper($0)}')
+
+  raw_value=$(cat $path_to_yaml_vars | shyaml get-value $key)
+
+  # some values in vars.yml use other variables that need to be dereferenced
+  dereferenced_value=$(eval echo $raw_value)
+  export $upper_case_key=$dereferenced_value
 done
+}
+
+unset_vars() {
+  for key in  ${LIST_OF_VARIABLES[@]}; do
+    # upper casing
+    var=$(echo $key | awk '{print toupper($0)}')
+    # unset the env var
+    unset $var
+  done
+}
+
+if  [[  $1 == 'unset' ]] ; then
+  unset_vars
+else
+  set_vars
+fi
