147d7878fbba23f7e9542751457931f9f0eb3b56
EJ Etherington <ej@reactiveops.com>
2017-12-27 15:33:59 -0800

    overriding compponent class for the inventory component, adding logic to not create ssh keys and stuff when it is not an aws account. remove vars.sh

--- pentagon/component/inventory/__init__.py
+++ pentagon/component/inventory/__init__.py
@@ -1,30 +1,72 @@
 import os
 import json
 import sys
+import logging
+import traceback
 
 from pentagon.component import ComponentBase
 from pentagon.component.vpc import Vpc
+from pentagon.helpers import create_rsa_key
+from pentagon.defaults import PentagonDefaults
 
 
 class Inventory(ComponentBase):
 
     _defaults = {'type': 'aws'}
 
+    def __init__(self, data, additional_args=None, **kwargs):
+        super(Inventory, self).__init__(data, additional_args, **kwargs)
+        self._ssh_keys = {
+                  'admin_vpn_key': self._data.get('admin_vpn_key', PentagonDefaults.ssh['admin_vpn_key']),
+                  'working_kube_key': self._data.get('working_kube_key', PentagonDefaults.ssh['working_kube_key']),
+                  'production_kube_key': self._data.get('production_kube_key', PentagonDefaults.ssh['production_kube_key']),
+                  'working_private_key': self._data.get('working_private_key', PentagonDefaults.ssh['working_private_key']),
+                  'production_private_key': self._data.get('production_private_key', PentagonDefaults.ssh['production_private_key']),
+                 }
+
     @property
     def _files_directory(self):
         return sys.modules[self.__module__].__path__[0] + "/files/common"
 
-    def add(self, destination):
+    def add(self, destination, overwrite=False):
+        """Inventory version of Component.add Copies files and templates from <component>/files and templates the *.jinja files """
         if destination == './':
-            destination = './default'
+            self._destination = './default'
+        else:
+            self._destination = destination
 
-        super(Inventory, self).add(destination)
+        self._overwrite = overwrite
 
-        if self._data['type'].lower() == 'aws':
-            Aws(self._data).add(destination)
+        try:
+            self._add_files()
 
-        if self._data['type'].lower() == 'gcp':
-            Gcp(self._data).add(destination)
+            if self._data['type'].lower() == 'aws':
+                self._data['aws_region'] = self._data.get('aws_default_region')
+                self._merge_data(self._ssh_keys)
+                self.__create_keys()
+                Aws(self._data).add(self._destination)
+
+            if self._data['type'].lower() == 'gcp':
+                Gcp(self._data).add(self._destination)
+
+            self._remove_init_file()
+            self._render_directory_templates()
+        except Exception as e:
+            logging.error("Error occured configuring component")
+            logging.error(e)
+            logging.debug(traceback.format_exc(e))
+            sys.exit(1)
+
+    def __create_keys(self):
+        key_path = "{}/{}/".format(self._destination, "config/private")
+
+        for key in self._ssh_keys:
+            logging.debug("Creating ssh key {}".format(key))
+            key_name = "{}".format(self._ssh_keys[key])
+            if not os.path.isfile("{}{}".format(key_path, key_name)):
+                create_rsa_key(key_name, key_path)
+            else:
+                logging.warn("Key {}{} exist!".format(key_path, key_name))
 
 
 class Aws(ComponentBase):
